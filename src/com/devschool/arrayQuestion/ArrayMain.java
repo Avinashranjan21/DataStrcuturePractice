c57185e609ab5b2d9e4713a0e3402827517f2288
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-10-02 16:18:46 +0530

    First question initialized

---
+++ src/com/devschool/arrayQuestion/ArrayMain.java
@@ -0,0 +1,14 @@
+package com.devschool.arrayQuestion;
+
+/**
+ * Created by avinash.ranjan on 02/10/17.
+ */
+public class ArrayMain {
+
+    public static void main(String[] args) {
+
+//        Calling sorting an array to even first
+        ArraySolution.SortEvenFirst();
+
+    }
+}
---
+++ src/com/devschool/arrayQuestion/ArraySolution.java
@@ -0,0 +1,22 @@
+package com.devschool.arrayQuestion;
+
+import java.util.Arrays;
+
+/**
+ * Created by avinash.ranjan on 02/10/17.
+ */
+public class ArraySolution {
+
+/*
+        Given an array A[], write a function that segregates even and odd numbers.
+        The functions should put all even numbers first, and then odd numbers.
+        Input  = {12, 34, 45, 9, 8, 90, 3}
+        Output = {12, 34, 8, 90, 45, 9, 3}
+ */
+
+
+    public static void SortEvenFirst() {
+    int a [] = {12, 34, 45, 9, 8, 90, 3};
+        System.out.println("Given array : a = " + Arrays.toString(a));
+    }
+}
